5563ddb002a56e3b7b25fa9d5b8fd4f85f6439a1
EshwarChatelli <eshwarchatelli@lewisu.edu>
2024-04-29 12:53:57 -0700

    Add project files.

---
+++ km to miles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter distance in kilometers:");
+        if (double.TryParse(Console.ReadLine(), out double kilometers))
+        {
+            double miles = kilometers * 0.621371;
+            Console.WriteLine($"{kilometers} kilometers is equal to {miles} miles.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+}
